11d583be74aa0b95fa49b285d7c9a6220280396a
Jay9011 <whdtjq5904@gmail.com>
2022-08-22 22:51:17 +0900

    퀵정렬, 삽입정렬, 선택정렬

--- SortAlgorithm/main.cpp
+++ SortAlgorithm/main.cpp
@@ -2,6 +2,19 @@
 
 using namespace std;
 
+void InsertionSort(int* arr, int size)
+{
+    for (int i = 1; i < size; i++)
+    {
+        int j = i;
+        while ( 0 < j && arr[j - 1] > arr[j])
+        {
+            swap(arr[j - 1], arr[j]);
+            j--;
+        }
+    }
+}
+
 void QuickSortUtil(int* arr, int lower, int upper)
 {
     if (upper <= lower) return;
@@ -37,11 +50,31 @@ void QuickSort(int* arr, int size)
     QuickSortUtil(arr, 0, size - 1);
 }
 
+void SelectionSort(int* arr, int size)
+{
+    int min;
+
+    for (int i = 0; i < size - 1; i++)
+    {
+        min = i;
+        for (int j = i + 1; j < size; j++)
+        {
+            if (arr[j] < arr[min])
+            {
+                min = j;
+            }
+        }
+        swap(arr[i], arr[min]);
+    }
+}
+
 int main()
 {
     int number[] = { 1, 10, 5, 8, 7, 6, 4, 3, 2, 9 };
 
     QuickSort(number, sizeof(number) / sizeof(*number));
+    //InsertionSort(number, sizeof(number) / sizeof(*number));
+    //SelectionSort(number, sizeof(number) / sizeof(*number));
 
     for (int i = 0; i < sizeof(number) / sizeof(*number); i++) 
     {
