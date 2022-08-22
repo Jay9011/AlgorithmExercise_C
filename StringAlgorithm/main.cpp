d33966345584a00db929e7d216888fe1d2c4e698
Jay9011 <whdtjq5904@gmail.com>
2022-08-22 19:50:13 +0900

    문자열 연결

--- StringAlgorithm/main.cpp
+++ StringAlgorithm/main.cpp
@@ -15,11 +15,34 @@ int strLen(char* str)
     return counter;
 }
 
+// 문자열 연결
+void strCat(char* dest, unsigned int size, char* src)
+{
+    int destSize = strLen(dest);
+    int srcSize = strLen(src);
+
+    for (int i = 0; i <= srcSize; i++)
+    {
+        if (src[i] == '\0') break;
+        if (size - 1 <= destSize + i)
+        {
+            dest[destSize + i] = '\0';
+            break;
+        }
+
+        dest[destSize + i] = src[i];
+    }
+}
+
 int main()
 {
-    char tmp[] = "test Text Code...";
+    char testText[19] = "test Text Code...";
+    char testPatt[] = "Text";
+    //cout << strLen(testText) << endl;
 
-    cout << strLen(tmp) << endl;
+    strCat(testText, 19, testPatt);
+
+    cout << testText << endl;
 
     return 0;
 }
