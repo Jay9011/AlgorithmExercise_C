2d8c6268138ef6c6c57606398dcaaf7c7b440bb6
Jay9011 <whdtjq5904@gmail.com>
2022-08-23 16:19:13 +0900

    에라토스테네스의 체 수정

--- Eratos/main.cpp
+++ Eratos/main.cpp
@@ -6,20 +6,21 @@ void EratosSieve(int n)
 {
     int* numbers = (int*)calloc(n + 1, sizeof(int));
 
+    if (n == 0) return;
+    
     for (int i = 0; i < n + 1; i++)
     {
         numbers[i] = i;
     }
-    for (int i = 2; i < n; i++)
+    for (int i = 2; i < n + 1; i++)
     {
         if (numbers[i] == 0) continue;
-        for (int j = 2 * i; j < n; j += i)
+        for (int j = 2 * i; j < n + 1; j += i)
         {
             numbers[j] = 0;
         }
     }
-    // 출력
-    for (int i = 2; i < n; i++)
+    for (int i = 1; i < n + 1; i++)
     {
         if (numbers[i] != 0)
         {
@@ -28,11 +29,12 @@ void EratosSieve(int n)
     }
 
     free(numbers);
+
 }
 
 int main()
 {
-    EratosSieve(50);
+    EratosSieve(47);    // 1 ~ 47까지 출력
 
     return 0;
 }
