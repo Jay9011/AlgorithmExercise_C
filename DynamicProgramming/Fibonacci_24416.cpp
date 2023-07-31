bf644bb72932a76dc3aeb2361d0c4f843206390e
Jay9011 <whdtjq5904@gmail.com>
2023-07-31 09:11:33 +0900

    DynamicProgramming

---
+++ DynamicProgramming/Fibonacci_24416.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+using namespace std;
+int counter1 = 0;
+int counter2 = 0;
+
+int fibR(int n)
+{
+	if (n == 1 || n == 2)
+	{
+		counter1++;
+		return 1;
+	}
+
+	return fibR(n - 1) + fibR(n - 2);
+}
+
+int fibD(int n)
+{
+	if (n == 1 || n == 2)
+		return 1;
+
+	int n1 = 1, n2 = 1, result = 1;
+	for(int i = 3; i <= n; i++)
+	{
+		n1 = n2;
+		n2 = result;
+		result = n1 + n2;
+		counter2++;
+	}
+
+	return result;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int N;
+	cin >> N;
+
+	fibR(N);
+	fibD(N);
+
+	cout << counter1 << ' ' << counter2;
+}
---
+++ DynamicProgramming/RecurToDynamic_9184.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <cstring>
+using namespace std;
+
+int result[101][101][101];
+
+int w(int a, int b, int c)
+{
+	if (a <= 0 || b <= 0 || c <= 0)
+		return result[a + 50][b + 50][c + 50] = 1;
+
+	if (result[a + 50][b + 50][c + 50] != -1)
+		return result[a + 50][b + 50][c + 50];
+
+	if (a > 20 || b > 20 || c > 20)
+		return result[a + 50][b + 50][c + 50] = w(20, 20, 20);
+
+	if(a < b && b < c)
+		return result[a + 50][b + 50][c + 50] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+
+	return result[a + 50][b + 50][c + 50] = w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	memset(result, -1, sizeof(result));
+
+	int a, b, c;
+	while(cin >> a >> b >> c)
+	{
+		if (a == -1 && b == -1 && c == -1) break;
+
+		cout << "w(" << a << ", " << b << ", " << c<< ") = " << w(a, b, c) << '\n';
+	}
+}
