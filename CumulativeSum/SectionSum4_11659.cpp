0e3b23845837fd6c6b568fbea21527fa9ed14e64
Jay9011 <whdtjq5904@gmail.com>
2023-08-15 19:17:24 +0900

    구간 합

---
+++ CumulativeSum/SectionSum4_11659.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int N, M;
+	cin >> N >> M;
+
+	vector<int> nums(N);
+	vector<int> prefixSum(N + 1, 0);
+
+	//구간합 미리 구해두기
+	for(int i = 0; i < N; i++)
+	{
+		cin >> nums[i];
+		prefixSum[i + 1] = prefixSum[i] + nums[i];
+	}
+
+	while(M--)
+	{
+		int i, j;
+		cin >> i >> j;
+		//두 구간의 구간합 차 사용
+		cout << prefixSum[j] - prefixSum[i-1] << '\n';
+	}
+}
