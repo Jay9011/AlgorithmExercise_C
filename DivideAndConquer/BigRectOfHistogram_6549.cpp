6c8285f340abbe096906b5817a217ea862d06a6f
Jay9011 <whdtjq5904@gmail.com>
2023-09-02 22:41:52 +0900

    히스토그램에서 가장 큰 직사각형

---
+++ DivideAndConquer/BigRectOfHistogram_6549.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+long long result;
+
+void solve(const vector<int>& arr, int s, int l, const int& e)
+{
+
+	for(int i = 0; i < (e - l); i++)
+	{
+		solve(arr, s + i, l - 1, e);
+	}
+
+	int minSize = arr[s];
+	for(int i = s + 1; i < l; i++)
+	{
+		if (minSize > arr[i]) minSize = arr[i];
+	}
+	if (result < (minSize * l)) result = (minSize * l);
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int N;
+	while (true)
+	{
+		cin >> N;
+		if (!N) return 0;
+
+		vector<int> nums(N, 0);
+
+		for(int i = 0; i < N; i++)
+		{
+			cin >> nums[i];
+		}
+
+		solve(nums, 0, N, N);
+	}
+}
