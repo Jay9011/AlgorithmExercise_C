ebfcf60bb40b2c72d91097c7eac8c43ab6843f9c
Jay9011 <whdtjq5904@gmail.com>
2023-09-03 18:21:39 +0900

    히스토그램에서 가장 큰 직사각형

--- DivideAndConquer/BigRectOfHistogram_6549.cpp
+++ DivideAndConquer/BigRectOfHistogram_6549.cpp
@@ -2,22 +2,42 @@
 #include <vector>
 using namespace std;
 
-long long result;
-
-void solve(const vector<int>& arr, int s, int l, const int& e)
+long long solve(const vector<int>& arr, int s, int e)
 {
+	// 기저사례: 하나의 직사각형의 크기
+	if (s == e - 1) return arr[s];
 
-	for(int i = 0; i < (e - l); i++)
+	// 분할: 두 개의 부분으로 나눈다.
+	int m = (s + e) / 2;
+
+	// 정복: 두 부분 중 큰 사각형을 찾는다.
+	long long result = max(solve(arr, s, m), solve(arr, m, e));
+
+	// 결합: 두 부분을 결합한 후, 중앙에서 시작한 직사각형의 넓이 중 최대값을 찾는다.
+	int l = m, r = m;
+	long long height = arr[m];
+	while(r - l < e - s)
 	{
-		solve(arr, s + i, l - 1, e);
+		// 경계까지 확장
+		long long lp = s < l ? arr[l - 1] : -1;
+		long long rp = r < e ? arr[r] : -1;
+
+		// 좀 더 큰 영역으로 확장
+		if(lp >= rp)
+		{
+			height = min(height, lp);
+			l--;
+		}
+		else
+		{
+			height = min(height, rp);
+			r++;
+		}
+
+		result = max(result, height * (r - l));
 	}
 
-	int minSize = arr[s];
-	for(int i = s + 1; i < l; i++)
-	{
-		if (minSize > arr[i]) minSize = arr[i];
-	}
-	if (result < (minSize * l)) result = (minSize * l);
+	return result;
 }
 
 int main()
@@ -38,6 +58,6 @@ int main()
 			cin >> nums[i];
 		}
 
-		solve(nums, 0, N, N);
+		cout << solve(nums, 0, N) << '\n';
 	}
 }
