e4b5f718ad44388c23900926529751d8c050645c
Jay9011 <whdtjq5904@gmail.com>
2023-09-09 23:18:14 +0900

    K번째 숫자

---
+++ BinarySearch/NumberK_ParametricSearch_1300.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+long long countLess(int N, long long num)
+{
+	long long cnt = 0;
+	for (int i = 1; i <= N; i++)
+	{
+		cnt += min(num / i, (long long)N);
+		if (num / i == 0) break;
+	}
+	return cnt;
+}
+
+long long searchK(int N, long long K)
+{
+	long long min = 1, max = (long long)N * N;
+
+	while (min <= max)
+	{
+		long long mid = (min + max) / 2;
+
+		if (K <= countLess(N, mid)) // mid보다 작거나 같은 수가 K보다 많다면
+		{
+			max = mid - 1;
+		}
+		else // mid보다 작거나 같은 수가 K보다 적다면
+		{
+			min = mid + 1;
+		}
+	}
+
+	return min;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int N;
+	long long k;
+	cin >> N >> k;
+
+	cout << searchK(N, k);
+}
