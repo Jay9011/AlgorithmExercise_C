db8a196dd93657f9cfa2bd8785da9d5d7b8c244f
Jay9011 <whdtjq5904@gmail.com>
2023-09-09 20:03:53 +0900

    공유기 설치

---
+++ BinarySearch/RouterInstall_ParametricSearch_2110.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool check(vector<int>& houses, int routerCnt, int m)
+{
+	int cnt = 1;
+	int last_house = houses[0];
+
+	for(int i = 1; i < houses.size(); i++)
+	{
+		if(houses[i] - last_house >= m)
+		{
+			cnt++;
+			last_house = houses[i];
+		}
+	}
+
+	return routerCnt <= cnt;
+}
+
+int maxLength(vector<int>& houses, int routerCnt)
+{
+	int result = 0;
+	int min = 1;
+	int max = houses.back() - houses.front();
+
+	while (min <= max)
+	{
+		int mid = (min + max) / 2;
+		if(check(houses, routerCnt, mid))
+		{
+			result = mid;
+			min = mid + 1;
+		}
+		else
+		{
+			max = mid - 1;
+		}
+	}
+
+	return result;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int N, C;
+	cin >> N >> C;
+
+	vector<int> houses(N);
+	for (int i = 0; i < N; i++)
+		cin >> houses[i];
+
+	sort(houses.begin(), houses.end());
+
+	cout << maxLength(houses, C);
+}
