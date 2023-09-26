8303c11dc0f0c91f51aa9ec4f8639fa63c7cdad1
Jay9011 <whdtjq5904@gmail.com>
2023-09-26 21:56:27 +0900

    웜 바이러스 문제

---
+++ Graph/Virus_2606.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int ConnectedCounter(int start, const vector<vector<int>>& network)
+{
+	int counter = 0;
+	vector<bool> visited(network.size(), false);
+	queue<int> Q;
+
+	// 시작 정점을 방문합니다.
+	Q.push(start);
+	visited[start] = true;
+
+	while (!Q.empty())
+	{
+		int curr = Q.front();
+		Q.pop();
+
+		for(int next : network[curr])
+		{
+			if (!visited[next])
+			{
+				Q.push(next);
+				visited[next] = true;
+				counter++;
+			}
+		}
+	}
+
+	return counter;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+
+	int N, E;
+	cin >> N >> E;
+
+	vector<vector<int>> network(N + 1);
+	while (E--)
+	{
+		int u, v;
+		cin >> u >> v;
+		network[u].push_back(v);
+		network[v].push_back(u);
+	}
+
+	cout << ConnectedCounter(1, network) << '\n';
+}
